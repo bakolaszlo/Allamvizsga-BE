aa7bc1f7ff7821d9fc7be918ae9392688316916a
bakolaszlo <79322093+bakolaszlo@users.noreply.github.com>
2022-03-12 13:51:51 +0200

    Created initial Migration.

---
+++ DataManager/Migrations/20220312115030_InitialMigration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DataManager.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Performances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    Gender = table.Column<string>(type: "text", nullable: false),
+                    CalibrationGrade = table.Column<string>(type: "text", nullable: false),
+                    TimeSpentToComplete = table.Column<int>(type: "integer", nullable: false),
+                    Pin = table.Column<string>(type: "text", nullable: false),
+                    ComplitionDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Performances", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCryptTable",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    EncryptedName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCryptTable", x => x.Name);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Performances");
+
+            migrationBuilder.DropTable(
+                name: "UserCryptTable");
+        }
+    }
+}
