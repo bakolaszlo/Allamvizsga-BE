3082719670ddeeab6c35c203486cfcd61dd421f0
bakolaszlo <79322093+bakolaszlo@users.noreply.github.com>
2022-03-12 13:49:33 +0200

    Added username to table.

--- DataManager/Model/Performance.cs
+++ DataManager/Model/Performance.cs
@@ -3,6 +3,7 @@
     public class Performance
     {
         public int Id { get; set; }
+        public string UserName { get; set; }
         public string Gender { get; set; }
         public string CalibrationGrade { get; set; }
         public int TimeSpentToComplete { get; set; }
