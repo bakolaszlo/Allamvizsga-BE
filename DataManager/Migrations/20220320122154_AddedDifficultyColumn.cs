c4cf3b5832eac90051922c528de1cbb134cbd2ca
bakolaszlo <79322093+bakolaszlo@users.noreply.github.com>
2022-03-20 14:25:28 +0200

    Added difficulty migration.

---
+++ DataManager/Migrations/20220320122154_AddedDifficultyColumn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataManager.Migrations
+{
+    public partial class AddedDifficultyColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Difficulty",
+                table: "Performances",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Difficulty",
+                table: "Performances");
+        }
+    }
+}
